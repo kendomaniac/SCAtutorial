362c5efae0f430446102f7d9a6e3c31b950d0b4f
kendomaniac <raffaele.calogero@gmail.com>
2020-08-20 07:09:24 +0200

    fixing some bugs

--- R/installing.rcasc.R
+++ R/installing.rcasc.R
@@ -1,6 +1,18 @@
-installing.rcasc <-
-function () 
-{
+#' @title A function prepare the environment for the SCA tutorial.
+#' @description This function check that dcker is installed and install locally rCASC library
+#' @author Raffaele Calogero, raffaele.calogero [at] unito [dot] it, University of Torino, Italy
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' installing.rcasc()
+#'
+#' }
+#'
+#' @export
+
+
+installing.rcasc <- function (){
     test <- system("docker -v", intern = TRUE)
     if (length(test) == 0) {
         cat("\nERROR: Docker seems not to be installed in your system\n")
@@ -11,6 +23,5 @@ function ()
             test, sep = ""))
         return(TRUE)
     }
-    require(devtools)
     install_github("kendomaniac/rCASC", ref = "master")
 }
